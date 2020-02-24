df118f1b474cccf7ed0b5878d0c4a28d9fa1768c
Nikola Gaić <nikola.gaic@outlook.com>
2020-02-24 23:14:36 +0100

    -

--- ElasticsearchTest/Controllers/IndexController.cs
+++ ElasticsearchTest/Controllers/IndexController.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
+using Nest;
 
 namespace ElasticsearchTest.Controllers
 {
@@ -15,18 +16,23 @@ namespace ElasticsearchTest.Controllers
     public class IndexController : ControllerBase
     {
         internal IContext _context;
+        internal IElasticClient _elasticClient;
 
-        public IndexController(IContext context)
+        public IndexController(IContext context, IElasticClient elasticClient)
         {
             _context = context;
+            _elasticClient = elasticClient;
         }
 
         [HttpPost()]
         [Route("")]
         public async Task<IActionResult> PostAsync()
         {
-            var addreses = await _context.Addresses.Take(50).ToListAsync();
-            addreses = await _context.Addresses.Skip(10).Take(50).ToListAsync();
+            var addreses = await _context.Addresses.Take(20).ToListAsync(); 
+
+            await _elasticClient.IndexDocumentAsync(addreses);
+
+            addreses = await _context.Addresses.Skip(20000).Take(20000).ToListAsync();
             return Ok();
         }
     }
